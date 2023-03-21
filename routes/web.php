63953c418a85c8b948578b24211d1296ded06dea
Baker <garbake>
2023-03-21 01:13:57 -0500

    Made changes to Web.php: Added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+
+Route::prefix('user')->group(function () {
+    //GET
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);  //GET Specific User
+
+    //POST
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']); 
+
+    //PUT OR PATCH
+    Route::get('/edit/{id}', [UserController::class, 'edit']);
+    Route::patch('/{id}', [UserController::class, 'update']); 
+
+    //DELETE
+    Route::delete('/{id}', [UserController::class, 'destroy']); 
 });
+
+
+
+//When a request is made to one of these registered routes, Laravel's router will automatically determine which method on the UserController class to call based on the HTTP verb (GET, POST, PUT, DELETE, etc.) and the URL parameters.
+//Route::resource('user', UserController::class);
+
